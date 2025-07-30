3dd269962406c8fd2cef9dd6bb557fcc52af8fb8
Amaravathi2002 <amaravathiettam@gmail.com>
2025-07-30 07:41:14 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,54 +1,21 @@
+from urllib.parse import quote_plus
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
-# Replace with your actual MongoDB connection URI
-client = MongoClient("mongodb+srv://<Amaravathi_2002>:<Amara_2002>@cluster0.mongodb.net/?retryWrites=true&w=majority")
+import os
+from dotenv import load_dotenv
+load_dotenv()
+app = Flask(__name__)
+username = quote_plus(os.getenv("Amaravathi_2002"))
+password = quote_plus(os.getenv("Amara_2002"))
+# MongoDB setup
+client = MongoClient(f"mongodb+srv://{username}:{password}@cluster0.karnc11.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client["todo_db"]
+todos = db["todos"]
 
-# Choose database and collection
-db = client["todo_database"]
-collection = db["todos"]
-
-# Menu
-def show_menu():
-    print("\n--- ToDo List Menu ---")
-    print("1. Add Task")
-    print("2. View Tasks")
-    print("3. Delete Task")
-    print("4. Exit")
-
-# Add Task
-def add_task():
-    task = input("Enter task: ")
-    collection.insert_one({"task": task})
-    print("Task added!")
-
-# View Tasks
-def view_tasks():
-    tasks = collection.find()
-    print("\n--- Your Tasks ---")
-    for t in tasks:
-        print(f"{t['_id']} - {t['task']}")
-
-# Delete Task
-def delete_task():
-    view_tasks()
-    task_id = input("Enter ID of task to delete: ")
-    from bson.objectid import ObjectId
-    collection.delete_one({"_id": ObjectId(task_id)})
-    print("Task deleted!")
-
-# Main Loop
-while True:
-    show_menu()
-    choice = input("Choose an option: ")
-
-    if choice == "1":
-        add_task()
-    elif choice == "2":
-        view_tasks()
-    elif choice == "3":
-        delete_task()
-    elif choice == "4":
-        print("Goodbye!")
-        break
-    else:
-        print("Invalid option.")
+# Display all tasks
+@app.route("/")
+def index():
+    all_todos = list(todos.find())
+    return render_template("index.html", todos=all_todos)
